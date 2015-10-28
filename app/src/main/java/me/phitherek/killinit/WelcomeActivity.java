3b318ce11cc7e6424cbed914de0913939f74be24
Phitherek_ <phitherek@gmail.com>
2015-10-28 10:35:48 +0100

    Added app source, pre-release.

---
+++ app/src/main/java/me/phitherek/killinit/WelcomeActivity.java
@@ -0,0 +1,29 @@
+package me.phitherek.killinit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://killinit.phitherek.me/users/sign_up"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+}
